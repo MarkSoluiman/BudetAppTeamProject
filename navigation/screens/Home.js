044ee1a661936cfd6db3b5772def525721512bb7
Sru03 <94092731+Sru03@users.noreply.github.com>
2023-04-03 17:28:47 +1200

    comments by Srushti

--- navigation/screens/Home.js
+++ navigation/screens/Home.js
@@ -5,7 +5,7 @@ export default function Home({navigation}){
     return(
         <View
         style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>  {/*style helps to locate the text in the centre of the screen */}
-            <Text>Home Screen</Text> {/* this will populate text named home on the hompe page */}
+            <Text>Home Screen</Text> {/* this will populate text named home on the hompe page  - comment by Srushti */}
         </View>
     )
 }
