b946944ce7ac15515b9b32a2c2e19ae64102403c
Jasmine Amohia <jasmine_amohia@hotmail.com>
2023-04-04 12:04:47 +1200

    Styled pages and navigation bar

--- navigation/screens/Advice.js
+++ navigation/screens/Advice.js
@@ -1,10 +1,9 @@
-import * as React from 'react'
 import { View, Text } from 'react-native'
 
 export default function Advice({navigation}){
     return(
         <View
-        style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+        style={{flex:1, backgroundColor: '#ffdeb7', alignItems: 'center', justifyContent: 'center'}}>
             <Text>Advice Screen</Text>
         </View>
     )
