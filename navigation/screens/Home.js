620bed65fd860747182acacdfdd94f07fc5556de
jasmimi <jasmine_amohia@hotmail.com>
2023-04-04 23:16:49 +1200

    Beginning to introduce widget modals to the homepage
    Navigate function of the modals isn't yet working WIP

--- navigation/screens/Home.js
+++ navigation/screens/Home.js
@@ -1,13 +1,19 @@
-import { StyleSheet, View, Text } from 'react-native'
+import React, { Component } from 'react'
+import { StyleSheet, View, Button, Text } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native'
+
+import BalanceModal from './BalanceModal'
 
 export default function Home({navigation}){
+    const modalNav = useNavigation()
+
     return(
         <SafeAreaView style={styles.background}>
             <ScrollView>
                 <View style={styles.widget}>
-                    <Text style={styles.text}>Current Balance</Text>
+                    <Text style={styles.text} onPress={() => modalNav.navigate("BalanceModal")}>Current Balance</Text>
                 </View>
                 <View style={styles.widget}>
                     <Text style={styles.text}>Monthly Spending</Text>
@@ -18,7 +24,7 @@ export default function Home({navigation}){
                 <View style={styles.widget}>
                     <Text style={styles.text}>Profit/Loss</Text>
                 </View>
-                <View style={styles.widget}> 
+                <View style={styles.widget}>
                     <Text style={styles.text}>Goal Bars</Text>
                 </View>
             </ScrollView>
@@ -34,16 +40,16 @@ const styles = StyleSheet.create({
         , justifyContent: 'center'
     },
     widget:{
-        marginHorizontal: 20,
-        marginBottom: 20,
-        borderRadius: 10,
-        borderColor: '#ff8100',
-        borderWidth: 8,
-        width: 370,
-        height: 250,
-        backgroundColor: '#ffe9de',
-        justifyContent: 'space-evenly',
-        fontWeight: 'bold'
+        marginHorizontal: 20
+        , marginBottom: 20
+        , borderRadius: 10
+        , borderColor: '#ff8100'
+        , borderWidth: 8
+        , width: 370
+        , height: 250
+        , backgroundColor: '#ffe9de'
+        , justifyContent: 'space-evenly'
+        , fontWeight: 'bold'
     },
     text:{
         textAlign: 'center',
