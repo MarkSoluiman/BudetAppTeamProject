2a816e5e19bda0c48474e46a51d74af005737385
Sru03 <94092731+Sru03@users.noreply.github.com>
2023-04-05 13:36:10 +1200

    changes made

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -71,3 +71,9 @@ export default function MainContainer(){
         </NavigationContainer>
     )
 }
+
+
+ 
+
+
+
--- navigation/screens/Login.js
+++ navigation/screens/Login.js
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text, KeyboardAvoidingView } from 'react-native'
 
 export default function Login({navigation}){
     return(
-        <View
+        <KeyboardAvoidingView style ={styles.container}
+        behaviour = "padding"> {/* this will help with keyboard populating on screen */}
+            <View
         style={{flex:1, backgroundColor: '#ffdeb7', alignItems: 'center', justifyContent: 'center'}}>
             <Text>Login Screen</Text>
         </View>
+        </KeyboardAvoidingView>
+
+
+
+
+       
     )
 }
+
+const styles = StyleSheet.create({
+
+
+    container:{
+
+        flex:1,
+        backgroundColor: '#fff',
+        alignItems:'center',
+        justifyContent: 'center',
+    },
+})
