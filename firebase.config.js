f2a03ec9e1afecbf2121d71075ac70b9a0b192b7
Sru03 <94092731+Sru03@users.noreply.github.com>
2023-04-10 11:37:58 +1200

    firebase setup config file and google-service.json file saved in android

---
+++ firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAPI468Wl9HNGcU3KJVtmMVq_Jy_DGEno",
+  authDomain: "mobile-budgeting-app.firebaseapp.com",
+  projectId: "mobile-budgeting-app",
+  storageBucket: "mobile-budgeting-app.appspot.com",
+  messagingSenderId: "647790618342",
+  appId: "1:647790618342:web:85533a4d00e9b94d523d27",
+  measurementId: "G-ZS77NTTZBT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
