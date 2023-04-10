a1327399ecd0a2098364bf9ed909d81cd6a24008
Sru03 <94092731+Sru03@users.noreply.github.com>
2023-04-10 12:10:39 +1200

    db auth and storage is grabbing 3 services from firebase and storing them into 3 variables

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { firebase } from "firebase";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +20,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = app.firestore();  {/* db auth and storage is grabbing 3 services from firebase and storing them into 3 variables */}
+const auth = firebase.auth();  
+const storage = firebase.storage();
+
+export {db, auth, storage};
