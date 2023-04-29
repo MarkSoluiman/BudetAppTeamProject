e51ff461ae806edeaa7d22dfeefd9047ac0447d3
Mark Soluiman <markashraf1030@gmail.com>
2023-04-29 17:51:33 +1200

    commented firebase.config
    commented firebase.config makes it work again

--- firebase.config.js
+++ firebase.config.js
@@ -1,30 +1,30 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { firebase } from "firebase";
-import { getFirestore } from "@firebase/firestore";
-import { getAuth } from "@firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { firebase } from "firebase";
+// import { getFirestore } from "@firebase/firestore";
+// import { getAuth } from "@firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDAPI468Wl9HNGcU3KJVtmMVq_Jy_DGEno",
-  authDomain: "mobile-budgeting-app.firebaseapp.com",
-  projectId: "mobile-budgeting-app",
-  storageBucket: "mobile-budgeting-app.appspot.com",
-  messagingSenderId: "647790618342",
-  appId: "1:647790618342:web:85533a4d00e9b94d523d27",
-  measurementId: "G-ZS77NTTZBT"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDAPI468Wl9HNGcU3KJVtmMVq_Jy_DGEno",
+//   authDomain: "mobile-budgeting-app.firebaseapp.com",
+//   projectId: "mobile-budgeting-app",
+//   storageBucket: "mobile-budgeting-app.appspot.com",
+//   messagingSenderId: "647790618342",
+//   appId: "1:647790618342:web:85533a4d00e9b94d523d27",
+//   measurementId: "G-ZS77NTTZBT"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-const db = getFirestore()  /* db auth and storage is grabbing 3 services from firebase and storing them into 3 variables */}
-const auth = getAuth(app)
-// const storage = firebase.storage();
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
+// const db = getFirestore()  /* db auth and storage is grabbing 3 services from firebase and storing them into 3 variables */}
+// const auth = getAuth(app)
+// // const storage = firebase.storage();
 
 
-export default {db, auth, storage,app};
+// export default {db, auth, storage,app};
