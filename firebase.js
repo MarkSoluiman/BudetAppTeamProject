e3f64eb2a030c38d43c06b03ffdc0b1e89ff83ca
Mark Soluiman <markashraf1030@gmail.com>
2023-05-03 17:52:42 +1200

    CRU in firebase done
     I still need to delete.

--- firebase.js
+++ firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore,getDocs } from "firebase/firestore/lite";
+import{firebase}from "firebase/compat/app"
 
 
 // Your web app's Firebase configuration
@@ -20,7 +21,11 @@ const firebaseConfig = {
 // Initialize Firebase
  const app=initializeApp(firebaseConfig)
  const auth=getAuth(app)
-
-
  const db =getFirestore(app)
+ 
+
+// if (!firebase.app.length){
+// firebase.initializeApp(firebaseConfig)
+// }
+
 export  {auth,db,app,firebaseConfig}
--- navigation/screens/Log.js
+++ navigation/screens/Log.js
@@ -4,31 +4,78 @@ import { TextInput } from "react-native-gesture-handler";
 import { useState } from "react";
 import LogItem from "./logComponents/LogItem";
 import LogInput from "./logComponents/LogInput";
-import { db } from "../../firebase";
-import { getDocs, collection } from "firebase/firestore/lite";
+import { db, app } from "../../firebase";
 
+import {
+  getDocs,
+  collection,
+  setDoc,
+  doc,
+  addDoc,
+  updateDoc,
+  QuerySnapshot,
+} from "firebase/firestore/lite";
+import { useEffect } from "react";
 
 // Exported function
 export default function Log() {
+  const date = new Date();
 
- 
-
-
-  async function getLogs() {
-    
-    const logsCol = collection(db, "Logs");
-    const logsSnapshot = await getDocs(logsCol);
-    const logsList = logsSnapshot.docs.map((doc) => doc.data());
-    console.log(logsList)
-  }
+  //to fetch every document as an array
   const [userLogs, setUserLogs] = useState([]);
+  //to fetch a singular user log as an object
+  //const [singleUserLog, setSingleUserLog] = useState({});
+  const [logValue, setLogValue] = useState("");
+  const [logDate, setLogDate] = useState("");
+  const userLogsRef = collection(db, "Logs");
+
+  //Read from firebase all of the logs
+  // async function getLogs() {
+  //   const logsCollection = collection(db, "Logs");
+  //   const data = await getDocs(logsCollection);
+  //   setUserLogs(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //   // console.log(userLogs.map((user)=>{
+  //   //   user.Description
+  //   // }))
+  //   console.log(
+  //     userLogs.map((user) => {
+  //       user.Description;
+  //     })
+  //   );
+  // }
 
   // Add to log
-  function addLogHandler(enteredLogText) {
-    setUserLogs((currentUserLogs) => [
-      ...currentUserLogs,
-      { text: enteredLogText, key: Math.random.toString() },
-    ]);
+  // function addLogHandler(enteredLogText) {
+  //   setUserLogs((currentUserLogs) => [
+  //     ...currentUserLogs,
+  //     { text: enteredLogText, key: Math.random.toString() },
+  //   ]);
+  // }
+
+  //reading from firebase:
+
+  useEffect(() => {
+    const getLogs = async () => {
+      const data = await getDocs(userLogsRef);
+      setUserLogs(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getLogs();
+  }, []);
+
+  //add log to firebase:
+  function createLog() {
+    addDoc(collection(db, "Logs"), {
+      Value: logValue,
+      Date: logDate,
+    });
+  }
+
+  //update log:
+  function updateLog() {
+    updateDoc(doc(db, "Logs", "log1"), {
+      Value: logValue,
+      Date: logDate,
+    });
   }
 
   // Delete from log
@@ -39,24 +86,69 @@ export default function Log() {
   }
 
   return (
-    
     <View style={styles.logContainer}>
-      <LogInput onAddLog={addLogHandler} />
-      <View style={styles.transactionsContainer}>
-        <Pressable onPress={getLogs}><Text>hello</Text></Pressable>
-        <FlatList
-          data={userLogs}
-          renderItem={(itemData) => {
-            return (
-              <LogItem
-                text={itemData.item.text}
-                id={itemData.item.id}
-                onDeleteItem={deleteLogHandler}
-              />
-            );
+      <View>
+        <TextInput
+          value={logValue}
+          onChangeText={(logValue) => {
+            setLogValue(logValue);
           }}
-        />
+          placeholder="logValue"
+        ></TextInput>
+
+        <TextInput
+          value={logDate}
+          onChangeText={(logDate) => {
+            setLogDate(logDate);
+          }}
+          placeholder="Date"
+        ></TextInput>
+
+        <Pressable onPress={createLog}>
+          <Text>Create</Text>
+        </Pressable>
       </View>
+
+      <View>
+        <TextInput
+          value={logValue}
+          onChangeText={(logValue) => {
+            setLogValue(logValue);
+          }}
+          placeholder="logValue"
+        ></TextInput>
+
+        <TextInput
+          value={logDate}
+          onChangeText={(logDate) => {
+            setLogDate(logDate);
+          }}
+          placeholder="Date"
+        ></TextInput>
+        <Pressable onPress={updateLog}>
+          <Text>update</Text>
+        </Pressable>
+
+        <View>
+          <Text>
+          {userLogs.map((userLog)=>{
+            return(
+              <Text>ID:{userLog.id}
+              Description:{userLog.Description}
+               Value:{userLog.Value}
+               Date:{userLog.Date}</Text>
+
+            )
+          }
+
+          )}
+          
+          </Text>
+        </View>
+      </View>
+      {/* <View>
+        <Pressable onPress={getLogs}><Text>get logs</Text></Pressable>
+      </View> */}
     </View>
   );
 }
