dc7b3f659e549889fdc44f57a3b614f5904f1ca6
Mark Soluiman <markashraf1030@gmail.com>
2023-05-04 18:11:54 +1200

    firebase file deleted

--- firebase.js
+++ firebase.js
@@ -1,31 +1,31 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore,getDocs } from "firebase/firestore/lite";
-import{firebase}from "firebase/compat/app"
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";// TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+// import { getFirestore,getDocs } from "firebase/firestore/lite";
+// import{firebase}from "firebase/compat/app"
 
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDAPI468Wl9HNGcU3KJVtmMVq_Jy_DGEno",
-  authDomain: "mobile-budgeting-app.firebaseapp.com",
-  projectId: "mobile-budgeting-app",
-  storageBucket: "mobile-budgeting-app.appspot.com",
-  messagingSenderId: "647790618342",
-  appId: "1:647790618342:web:85533a4d00e9b94d523d27",
-  measurementId: "G-ZS77NTTZBT"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDAPI468Wl9HNGcU3KJVtmMVq_Jy_DGEno",
+//   authDomain: "mobile-budgeting-app.firebaseapp.com",
+//   projectId: "mobile-budgeting-app",
+//   storageBucket: "mobile-budgeting-app.appspot.com",
+//   messagingSenderId: "647790618342",
+//   appId: "1:647790618342:web:85533a4d00e9b94d523d27",
+//   measurementId: "G-ZS77NTTZBT"
+// };
 
 
-// Initialize Firebase
- const app=initializeApp(firebaseConfig)
- const auth=getAuth(app)
- const db =getFirestore(app)
+// // Initialize Firebase
+//  const app=initializeApp(firebaseConfig)
+//  const auth=getAuth(app)
+//  const db =getFirestore(app)
  
 
-// if (!firebase.app.length){
-// firebase.initializeApp(firebaseConfig)
-// }
+// // if (!firebase.app.length){
+// // firebase.initializeApp(firebaseConfig)
+// // }
 
-export  {auth,db,app,firebaseConfig}
+// export  {auth,db,app,firebaseConfig}
