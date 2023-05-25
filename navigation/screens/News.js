6d5e252df755b7a6412379836db896ecd66a5d6a
shambhavi152 <77769134+shambhavi152@users.noreply.github.com>
2023-05-25 14:13:28 +1200

    updated the query

--- navigation/screens/News.js
+++ navigation/screens/News.js
@@ -6,36 +6,38 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function News({ navigation }) {
   const [data, setData] = useState([]);
-  const[searchQuery,setSearchQuery]=useState('');
+  const [searchQuery, setSearchQuery] = useState('');
 
   const url =
     'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=044175598afd464db1a017c29900c328';
-
-  const getArticles = () => {
-    fetch(url)
-      .then(res => res.json())
-      .then(json => setData(json))
-      .catch(err => console.log(err));
-  };
 
   useEffect(() => {
     getArticles();
   }, []);
 
-  const handleSearch =() =>{
-    const filteredArticles = data.articles.filter(
-      article=>article.title)
+  const getArticles = () => {
+    fetch(url)
+      .then(res => res.json())
+      .then(json => setData(json.articles))
+      .catch(err => console.log(err));
+  };
 
-      return filteredArticles;
-    )
-  }
+  const handleSearch = text => {
+    setSearchQuery(text);
+  };
+
+  const filteredArticles = data.filter(article =>
+    article.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   const SearchBar = () => {
     return (
       <View>
-        <TextInput placeholder="Search News" style={styles.input} 
-        style={styles.input}
-        Text => setSearchQuery(TextInput)}
+        <TextInput
+          placeholder="Search News"
+          style={styles.input}
+          value={searchQuery}
+          onChangeText={handleSearch}
         />
       </View>
     );
@@ -46,27 +48,26 @@ export default function News({ navigation }) {
       <ScrollView>
         <View style={styles.container}>
           <SearchBar />
-          {Object.keys(data).length > 0 &&
-            data.articles.map((article, index) => (
-              <TouchableOpacity
-                key={index}
-                onPress={() => navigation.navigate('ArticleDetail', { article })}
-              >
-                <Card style={styles.card}>
-                  <Image source={{ uri: article.urlToImage }} style={styles.image} />
-                  <View style={styles.articleContainer}>
-                    <Text style={styles.title}>{article.title}</Text>
-                  </View>
-                  <Text style={styles.description}>{article.description}</Text>
-                  <View style={styles.heading}>
-                    <Text style={styles.source}>{article.source.name}</Text>
-                  </View>
-                  <View style={styles.heading}>
-                    <Text style={styles.date}>{article.publishedAt}</Text>
-                  </View>
-                </Card>
-              </TouchableOpacity>
-            ))}
+          {filteredArticles.map((article, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => navigation.navigate('ArticleDetail', { article })}
+            >
+              <Card style={styles.card}>
+                <Image source={{ uri: article.urlToImage }} style={styles.image} />
+                <View style={styles.articleContainer}>
+                  <Text style={styles.title}>{article.title}</Text>
+                </View>
+                <Text style={styles.description}>{article.description}</Text>
+                <View style={styles.heading}>
+                  <Text style={styles.source}>{article.source.name}</Text>
+                </View>
+                <View style={styles.heading}>
+                  <Text style={styles.date}>{article.publishedAt}</Text>
+                </View>
+              </Card>
+            </TouchableOpacity>
+          ))}
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -99,14 +100,14 @@ const styles = StyleSheet.create({
     borderColor: '#ff8100',
   },
   title: {
-    width: Dimensions.get('window').width -10,
+    width: Dimensions.get('window').width - 10,
     marginTop: 1,
     fontSize: 18,
     fontWeight: '600',
     flex: 1,
   },
   image: {
-    width: Dimensions.get('window').width-10,
+    width: Dimensions.get('window').width - 10,
     height: 200,
     alignItems: 'center',
   },
