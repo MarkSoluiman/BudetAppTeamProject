87c999676c3dea9e3db79e92b995a8f517dce076
jasmimi <jasmine_amohia@hotmail.com>
2023-05-28 09:49:52 +1200

    Update existing goals functioning and commented

--- navigation/screens/GoalsModal.js
+++ navigation/screens/GoalsModal.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, Pressable, Platform, Alert } from 'react-native
 import React, { useState, useEffect } from 'react'
 import DateTimePicker from '@react-native-community/datetimepicker'
 import { TextInput } from 'react-native-gesture-handler'
-import { collection, addDoc } from 'firebase/firestore/lite'
+import { collection, addDoc, updateDoc, doc } from 'firebase/firestore/lite'
 import { db, firebase } from '../../firebase.config'
 import { getAuth } from 'firebase/auth'
 
@@ -17,26 +17,31 @@ export default function GoalsModal({navigation, route}){
     const [showPicker, setShowPicker] = useState(false)
     const [goalDate, setGoalDate] = useState("Select your goal completion date")
 
+    // Hook useEffect checks if user was navigated to page with route parameters, if the user wants to update a document
     useEffect(() => {
         if (route.params) {
+
+            // Assign parameter as goal document ID
             const { goalID } = route.params;
-            console.log(goalID);
+
+            // Query to find goal document in firebase from assigned ID
             firebase
                 .firestore()
                 .collection('Goals')
                 .doc(goalID)
                 .get()
                 .then((documentSnapshot) => {
-                    console.log(documentSnapshot);
                     if (documentSnapshot.exists) {
-                        console.log('exists', documentSnapshot.data());
+
+                        // Set entry field variables to corresponding values in firebase
                         setName(documentSnapshot.data().goal_name);
                         setAmount(documentSnapshot.data().goal_amount);
                         const goalDateTimestamp = documentSnapshot.data().goal_date;
                         const goalDate = goalDateTimestamp.toDate(); // Convert timestamp to Date object
                         setDate(goalDate);
                         setGoalDate(goalDate.toDateString());
-                        console.log('date', goalDate, 'name', newName, 'amount', newAmount);
+
+                    // Possible error messages
                     } else {
                         console.log('Document not found!');
                     }
@@ -46,7 +51,10 @@ export default function GoalsModal({navigation, route}){
                 });
         }
     }, []);
-    
+
+    // Constants used to determine if app should render components for a new goal to add, or an existing goal to update
+    const shouldRenderName = route.params; 
+    const shouldRenderAmount = !!route.params;
  
     // Initialise date picker for goal completed by date
     const toggleDatepicker = () => {
@@ -67,7 +75,56 @@ export default function GoalsModal({navigation, route}){
         }
     }
 
-    // Validate entry input, if successful... write to firebase
+    // Function is called on completion of a goal update, determines goal status with updated goal balance and amount, notifies user of goal completion if valid and necessary
+    const goalNotifications = async (goalID) => {
+
+        // Find if goal met, goal_balance >= goal_met
+        firebase
+            .firestore()
+            .collection("Goals").
+            doc(goalID)
+            .get()
+            .then((documentSnapshot) => {
+                if (documentSnapshot.exists){
+                    if (documentSnapshot.data().goal_balance >= documentSnapshot.data().goal_amount){
+
+                        // Change goal status to true if complete
+                        const goalRef = doc(db, 'Goals', goalID)
+                        try{
+                            updateDoc(goalRef, {
+                                goal_complete: true
+                            })
+                            console.log('Goal status updated successfully')
+
+                            // Find if goal notifications are on, if yes, alert user of goal completion
+                            firebase.firestore().collection("Profile").where('uid', '==', getAuth().currentUser.uid).where("notifications", "==", true).get()
+                            .then(querySnapshot => {
+                                querySnapshot.forEach(documentSnapshot => {
+                                    if (documentSnapshot.data()['notifications'] === true){
+                                        Alert.alert("Goal completed!")
+                                        console.log("Goal completed as a console log")
+                                    }
+                                })
+                            })
+
+                            // Potential error messages
+                            .catch((error) => {
+                                console.log("Error getting documents: ", error)
+                            })                            
+                        } catch (error){
+                            console.log('Goal document updated unsuccessfully')
+                        }
+                    }
+                } else {
+                    console.log('Document not found!')
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+            })        
+    }
+
+    // For new goals... validate entry input, if successful... write to firebase
     const handleSubmit = async () => {
         if(newName.length > 0){
             if (Number.isInteger(parseInt(newAmount)) && parseInt(newAmount) > 0) {
@@ -97,8 +154,40 @@ export default function GoalsModal({navigation, route}){
         }
     }
 
-    const shouldRenderName = route.params; 
-    const shouldRenderAmount = !!route.params;
+    // For existing goals to update... validate entry input, if successful... write to firebase
+    const handleUpdate = async (goalID) => {
+        if(newName.length > 0){
+            if(newName.length > 0){
+                if (Number.isInteger(parseInt(newAmount)) && parseInt(newAmount) > 0) {
+                    if(date > new Date()){
+                        amount = parseInt(newAmount, 10)
+    
+                        navigation.navigate('Goals')
+    
+                        const goalRef = doc(db, 'Goals', goalID)
+                        try{
+                            await updateDoc(goalRef, {
+                                goal_name: newName,
+                                goal_amount: amount,
+                                goal_date: date,
+                            })
+                            console.log('Goal document updated successfully')
+                            Alert.alert('Goal updated')
+
+                            goalNotifications(goalID)
+                        } catch (error){
+                            console.log('Goal document updated unsuccessfully')
+                        }
+                    } else {
+                        Alert.alert('Error: Goal date must be in the future')
+                    }
+                } else {
+                    Alert.alert('Error: Goal amount must be an integer greater than 0')
+                }
+            } else {
+                Alert.alert('Error: Goal name must be of length greater than 0')
+            }
+    }}
 
     // Exported function
     return(
@@ -140,12 +229,22 @@ export default function GoalsModal({navigation, route}){
 
             {/* Buttons to save or cancel goal data entry */}
             <View style={styles.buttons}>
+
+                {/* Cancel goal data entry */}
                 <Pressable style={styles.button} onPress={()=> navigation.navigate('Goals')}>
                     <Text style={styles.prompts}>BACK</Text>
                 </Pressable>
-                <Pressable style={styles.button} onPress={handleSubmit}>
-                    <Text style={styles.prompts}>SAVE</Text>
-                </Pressable>
+
+                {/* Update existing goal OR add new goal */}
+                {shouldRenderName ? (
+                    <Pressable style={styles.button} onPress={() => handleUpdate(route.params.goalID)}>
+                        <Text style={styles.prompts}>UPDATE</Text>
+                    </Pressable>
+                ) : (
+                    <Pressable style={styles.button} onPress={handleSubmit}>
+                        <Text style={styles.prompts}>SAVE</Text>
+                    </Pressable>
+                )}
             </View>
         </View>
     )
@@ -153,6 +252,8 @@ export default function GoalsModal({navigation, route}){
 
 // Styling
 const styles = StyleSheet.create({
+
+    // Page styling
     background:{
         flex:1
         , backgroundColor: '#ffdeb7'
@@ -176,6 +277,8 @@ const styles = StyleSheet.create({
         , fontWeight: 'bold'
         , fontSize: 17
     },
+
+    // Button styling
     buttons: {
         justifyContent: 'center'
         , flexDirection: 'row'
