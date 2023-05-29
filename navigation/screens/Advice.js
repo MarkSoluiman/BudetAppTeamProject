6c7b108fc79150891ad61a4374ca83a8d217f087
Shambhavi Bhadauria <shambhavi152@gmail.com>
2023-05-30 06:33:32 +1200

    creating the chat section

--- navigation/screens/Advice.js
+++ navigation/screens/Advice.js
@@ -1,50 +1,44 @@
 // Component imports
-import { SafeAreaView, ScrollView, View, Text, StyleSheet, Linking } from 'react-native'
+import React from 'react';
+import { SafeAreaView, ScrollView, View, StyleSheet} from 'react-native';
 
 // Exported function
-export default function Advice({navigation}){
-    return(
+export default function Advice({navigation}) {
+    return (
         <SafeAreaView style={styles.background}>
             <ScrollView>
-
-              
-
+                <View style={styles.container}></View>
             </ScrollView>
         </SafeAreaView>
-    )
+    );
 }
 
 // Styling
 const styles = StyleSheet.create({
-    background:{
-        flex:1
-        , paddingTop: 20
-        , backgroundColor: '#ffdeb7'
+    container: {
+        flex: 1,
     },
-    widget:{
-        marginHorizontal: 20
-        , marginBottom: 20
-        , borderRadius: 10
-        , borderColor: '#ff8100'
-        , borderWidth: 3
-        , width: 370
-        , padding: 15
-        , backgroundColor: '#ffe9de'
-        , justifyContent: 'space-evenly'
+    background: {
+        flex: 1,
+        paddingTop: 20,
+        backgroundColor: '#ffdeb7',
     },
-    heading:{
-        fontWeight: 'bold'
-        , fontSize: 16
-        , textAlign: 'center'
+    widget: {
+        marginHorizontal: 20,
+        marginBottom: 20,
+        borderRadius: 10,
+        borderColor: '#ff8100',
+        borderWidth: 3,
+        width: 370,
+        padding: 15,
+        backgroundColor: '#ffe9de',
+        justifyContent: 'space-evenly',
     },
-    article:{
-        fontSize: 14
+    footerWidget: {
+        marginHorizontal: 20,
+        marginBottom: 20,
+        width: 370,
+        padding: 15,
+        justifyContent: 'space-evenly',
     },
-    footerWidget:{
-        marginHorizontal: 20
-        , marginBottom: 20
-        , width: 370
-        , padding: 15
-        , justifyContent: 'space-evenly'
-    }
-})
+});
