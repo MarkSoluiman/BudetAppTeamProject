907e36010dd9e69293b403aebf28759dfb544b38
Shambhavi Bhadauria <shambhavi152@gmail.com>
2023-05-30 07:12:15 +1200

    styling the chatbox

--- navigation/screens/Chatbox.js
+++ navigation/screens/Chatbox.js
@@ -1,17 +1,16 @@
-import React,{useState} from 'react'
-import {View, Text, StyleSheet,Flatlist,Textinput} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList, TextInput } from 'react-native';
 import axios from 'axios';
-
 
 const ChatGPT = () => {
     const [data, setData] = useState([]);
     const apikey = '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';
     const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
-    const [textInput,setTextinput] = useState('');
+    const [textInput, setTextInput] = useState('');
 
-    const handleSet = async () => {
+    const handleSend = async () => {
         const prompt = textInput;
-        try{
+        try {
             const response = await axios.post(
                 apiUrl,
                 {
@@ -20,39 +19,74 @@ const ChatGPT = () => {
                     temperature: 0.5,
                 },
                 {
-                    headers:
-                    'Content-Type': 'advice/json',
-                    'Authorisation': 'Bearer ${apikay}',
-                },  
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${apikey}`,
+                    },
+                }
             );
 
-            const text = response.data.choice[0].text;
-            setData([...data {type: 'user',text: textinput},{type: 'bot',text: text}]);
+            const text = response.data.choices[0].text;
+            setData([...data, { type: 'user', text: textInput }, { type: 'bot', text: text }]);
             setTextInput('');
-    }catch(error){
-        console.log(error);
-    }
-} ;
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-return(
-    <View styles={styles.container}>
-        <Text style={styles.title}>AI Advice ChatBox</Text>
-        <Flatlist
-          data={data}
-          keyExtractor={(item,index)=> index.toString()}
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>AI Advice ChatBox</Text>
+            <FlatList
+                data={data}
+                keyExtractor={(item, index) => index.toString()}
+                style={styles.body}
+                renderItem={({ item }) => (
+                    <View style={{ flexDirection: 'row', padding: 10 }}>
+                        <Text style={{ fontWeight: '600', color: item.type === 'user' ? 'green' : 'red' }}>
+                            {item.type === 'user' ? 'User' : 'Bot'}
+                        </Text>
+                        <Text style={styles.bot}>{item.text}</Text>
+                    </View>
+                )}
+            />
+            <TextInput
+                style={styles.input}
+                value={textInput}
+                onChangeText={(text) => setTextInput(text)}
+                placeholder="Ask me for advice"
+                onSubmitEditing={handleSend}
+            />
+        </View>
+    );
+};
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: '600',
+        marginTop: 50,
+        marginBottom: 20,
+        textAlign: 'center',
+    },
+    body: {
+        backgroundColor: '#fff',
+    },
+    bot: {
+        flex: 1,
+        paddingLeft: 10,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingHorizontal: 10,
+        width: '90%',
+    },
+});
 
-    </View>
-      
-)
-
-
-
-
-
-
-
-
-const styles=StyleSheet.create({
-
-})
+export default ChatGPT;
