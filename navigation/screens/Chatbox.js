5e6f144b7bd22e949467a7c34935fef16c14a18f
Shambhavi <107373811+Shamy152@users.noreply.github.com>
2023-05-31 10:12:36 +1200

    Updated

--- navigation/screens/Chatbox.js
+++ navigation/screens/Chatbox.js
@@ -1,61 +1,53 @@
-const { Touchable, TouchableOpacity } = require("react-native");
-const { TextInput } = require("react-native-gesture-handler");
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { OpenAIApi, configuration } from 'openai';
 
-const configuration = new configuration ({
-  apiKey: process.env.OPEN_API_KEY,
-
-});
-
-const openai = new OpenAIApi(configuration);
-
-const Chatbox =() => {
-  const [input,setInput] = useState('');
-  const [output,setOutput] = useState('');
+const Chatbox = () => {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
 
   const handleInput = async () => {
     try {
-      const userInput = constPrompt + input
-      const response = await openai.createCompletion({   // response const = openai  ...
-        model: "text-davinci-003" ////openai completion method creates the code to the api and fetches the response for the user.
-        prompt: 'You: ${userInput}\nAI:',
+      const userInput = `You: ${input}\nAI:`;
+      const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: userInput,
         temperature: 0,
         max_tokens: 60,
         top_p: 2.0,
         frequency_penalty: 0.5,
         presence_penalty: 0.0,
-        stop: ["You:"],
-
+        stop: ['You:'],
       });
-      setOutput(response.data.choices[0].text);  
-
-    }catch (error)
-    {
+      setOutput(response.data.choices[0].text);
+    } catch (error) {
       console.log(error);
     }
 
     setInput('');
-  }
+  };
 
-  return(
+  return (
     <View style={styles.container}>
-      <Text style={styles.title}> Advice ChatBot</Text> // name of the chat container
+      <Text style={styles.title}>Advice ChatBot</Text>
       <View style={styles.chatContainer}>
-           <View style={styles.inputContainer}>
-              <TextInput
-                 style={styles.input}
-                 placeholder="Please enter your question!"
-                 onChange={(text) => setInput(text)}
-                 value={input}
-
-                 />
-                 <TouchableOpacity style={styles.sendButton} onPress={handleInput}>  /// handle input is used each time to send the question
-                       <Text style={styles.sendButtonText}>Send</Text>
-                 </TouchableOpacity>
-                 <View style={styles.outputContainer}>
-                      <Text style={styles.output}>{output}</Text>
-                 </View>
-           </View>        
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Please enter your question!"
+            onChangeText={(text) => setInput(text)}
+            value={input}
+          />
+          <TouchableOpacity style={styles.sendButton} onPress={handleInput}>
+            <Text style={styles.sendButtonText}>Send</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.outputContainer}>
+          <Text style={styles.output}>{output}</Text>
+        </View>
       </View>
     </View>
-  )
-}
+  );
+};
+
+export default Chatbox;
