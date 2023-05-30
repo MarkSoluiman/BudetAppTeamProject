6aa86de12e5e62811f3f6b058847ce6d80594c0d
Shambhavi <107373811+Shamy152@users.noreply.github.com>
2023-05-30 21:57:17 +1200

    Updating the api and hiding it while fixing how the ui of the chatbox looks like

--- navigation/screens/Chatbox.js
+++ navigation/screens/Chatbox.js
@@ -1,98 +1,107 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList, TextInput } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TextInput, TouchableOpacity } from 'react-native';
 import axios from 'axios';
+import { OpenAIApi } from 'openai';
 
 const ChatGPT = () => {
-    const [data, setData] = useState([]);
-    const apikey = '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';
-    const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
-    const [textInput, setTextInput] = useState('');
+  const [data, setData] = useState([]);
+  const [textInput, setTextInput] = useState('');
 
-    const handleSend = async () => {
-        const prompt = textInput;
-        try {
-            const response = await axios.post(
-                apiUrl,
-                {
-                    prompt: prompt,
-                    max_tokens: 1024,
-                    temperature: 0.5,
-                },
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${apikey}`,
-                    },
-                }
-            );
+  const handleSend = async () => {
+    const prompt = textInput;
+    try {
+      const apikey = process.env.OPENAI_API_KEY;
+      const openaiInstance = new OpenAIApi(apikey);
 
-            const text = response.data.choices[0].text;
-            setData([...data, { type: 'user', text: textInput }, { type: 'bot', text: text }]);
-            setTextInput('');
-        } catch (error) {
-            console.log(error);
-        }
-    };
+      const response = await openaiInstance.complete({
+        engine: 'text-davinci-003',
+        prompt: prompt,
+        maxTokens: 1024,
+        temperature: 0.5,
+      });
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>AI Advice ChatBox</Text>
-            <View style={styles.chatContainer}>
-                <FlatList
-                    data={data}
-                    keyExtractor={(item, index) => index.toString()}
-                    style={styles.body}
-                    renderItem={({ item }) => (
-                        <View style={{ flexDirection: 'row', padding: 10 }}>
-                            <Text style={{ fontWeight: '600', color: item.type === 'user' ? 'green' : 'red' }}>
-                                {item.type === 'user' ? 'User' : 'Bot'}
-                            </Text>
-                            <Text style={styles.bot}>{item.text}</Text>
-                        </View>
-                    )}
-                />
-                <TextInput
-                    style={styles.input}
-                    value={textInput}
-                    onChangeText={(text) => setTextInput(text)}
-                    placeholder="Ask me for advice"
-                    onSubmitEditing={handleSend}
-                />
+      const text = response.choices[0].text;
+      setData([...data, { type: 'user', text: textInput }, { type: 'bot', text: text }]);
+      setTextInput('');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>AI Advice ChatBox</Text>
+      <View style={styles.chatContainer}>
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) => index.toString()}
+          style={styles.body}
+          renderItem={({ item }) => (
+            <View style={{ flexDirection: 'row', padding: 10 }}>
+              <Text style={{ fontWeight: '600', color: item.type === 'user' ? 'green' : 'red' }}>
+                {item.type === 'user' ? 'User' : 'Bot'}
+              </Text>
+              <Text style={styles.bot}>{item.text}</Text>
             </View>
-        </View>
-    );
+          )}
+        />
+        <TextInput
+          style={styles.input}
+          value={textInput}
+          onChangeText={(text) => setTextInput(text)}
+          placeholder="Ask me for advice"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleSend}>
+          <Text style={styles.buttonText}>Send</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: '600',
-        marginTop: 50,
-        marginBottom: 20,
-        textAlign: 'center',
-    },
-    chatContainer: {
-        flex: 1,
-        paddingHorizontal: 20,
-    },
-    body: {
-        backgroundColor: '#fff',
-    },
-    bot: {
-        flex: 1,
-        paddingLeft: 10,
-    },
-    input: {
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginBottom: 10,
-        paddingHorizontal: 10,
-        width: '100%',
-    },
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    marginTop: 50,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  chatContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  body: {
+    backgroundColor: '#ffdeb7',
+  },
+  bot: {
+    flex: 1,
+    paddingLeft: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: '#ffdeb7',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    width: '100%',
+  },
+  button: {
+    backgroundColor: 'orange',
+    width: '90%',
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: 'white',
+  },
 });
 
 export default ChatGPT;
