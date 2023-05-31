dd0470d3e00670e4e14856f77ed1b60031f9ca84
Shambhavi <107373811+Shamy152@users.noreply.github.com>
2023-06-01 11:16:55 +1200

    update

--- navigation/screens/Advice.js
+++ navigation/screens/Advice.js
@@ -1,35 +1,4 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
-import { OpenAI } from 'openai';
 
-const openai = new OpenAI({
-  apiKey: process.env.OPEN_API_KEY,
-});
-
-const Advice = () => {
-  const [input, setInput] = useState('');
-  const [output, setOutput] = useState('');
-
-  const handleInput = async () => {
-    try {
-      const userInput = `You: ${input}\nAI:`;
-      const response = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: userInput,
-        temperature: 0,
-        max_tokens: 60,
-        top_p: 2.0,
-        frequency_penalty: 0.5,
-        presence_penalty: 0.0,
-        stop: ['You:'],
-      });
-      setOutput(response.data.choices[0].text);
-    } catch (error) {
-      console.log(error);
-    }
-
-    setInput('');
-  };
 
   return (
     <View style={styles.container}>
@@ -57,47 +26,6 @@ const Advice = () => {
 export default Advice;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  chatContainer: {
-    width: '80%',
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    marginBottom: 10,
-  },
-  input: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: 'gray',
-    padding: 10,
-  },
-  sendButton: {
-    backgroundColor: 'blue',
-    marginLeft: 10,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 5,
-  },
-  sendButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  outputContainer: {
-    backgroundColor: 'lightgray',
-    padding: 10,
-  },
-  output: {
-    fontSize: 16,
-  },
   background: {
     flex: 1,
     paddingTop: 20,
