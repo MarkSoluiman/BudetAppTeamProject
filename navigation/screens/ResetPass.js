78f9e2ad71827abad043b6706c2df7fe91f27fdb
jasmimi <jasmine_amohia@hotmail.com>
2023-06-02 12:39:40 +1200

    Commenting and code refinement for Reset Password page

--- navigation/screens/ResetPass.js
+++ navigation/screens/ResetPass.js
@@ -1,113 +1,123 @@
+//  Import components
 import React, { useState } from "react";
 import { StyleSheet, View, Text, TextInput, Pressable, Alert } from "react-native";
 import { initializeApp } from "firebase/app";
 import { getAuth, sendPasswordResetEmail, generatePasswordResetLink } from "firebase/auth";
-
 const auth = getAuth();
 
 // Exported function
 export default function ResetPass({ navigation }) {
-  const [email, setEmail] = useState("");
 
-  const actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for
-    // this URL must be whitelisted in the Firebase Console.
-    url: "https://www.example.com/checkout?cartId=1234",
-    // This must be true for email link sign-in.
-    handleCodeInApp: true,
-    iOS: {
-      bundleId: "com.example.ios",
-    },
-    android: {
-      packageName: "com.example.android",
-      installApp: true,
-      minimumVersion: "12",
-    },
-    // FDL custom domain.
-    dynamicLinkDomain: "coolapp.page.link",
-  };
+    // Initialise constants
+    const [email, setEmail] = useState("");
 
-  const handleReset = () => {
-    if (email.length > 0) {
-        sendPasswordResetEmail(auth, email)
-        .then(() => {
-          Alert.alert("Password reset email sent successfully!");
-        })
-        .catch((error) => {
-          Alert.alert("Error: " + error.message);
-        });
-    } else {
-      Alert.alert("Please enter an email");
-    }
-  };
+    // Function called when user presses submit button
+    const handleReset = () => {
 
-  return (
-    <View style={styles.background}>
-      <Text style={styles.headingText}>RESET PASSWORD</Text>
-      <Text style={styles.subHeadingText}>ENTER EMAIL</Text>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Email"
-        onChangeText={(value) => setEmail(value)}
-      />
-      <View style={styles.buttons}>
-        <Pressable style={styles.button}>
-          <Text style={styles.buttonText} onPress={() => navigation.navigate("Login")}>
-            BACK
-          </Text>
-        </Pressable>
-        <Pressable style={styles.button} onPress={handleReset}>
-          <Text style={styles.buttonText}>RESET</Text>
-        </Pressable>
-      </View>
-    </View>
-  );
+        // Input validation, a string must be entered
+        if (email.length > 0) {
+
+            // Use authorisation and email to send a password reset email
+            sendPasswordResetEmail(auth, email)
+
+            // Success message
+            .then(() => {
+                Alert.alert("Password reset email sent successfully!");
+            })
+
+            // Unsuccessful message eg. invalid email
+            .catch((error) => {
+                Alert.alert("Error: " + error.message);
+            });
+        
+        // Input validation alert message
+        } else {
+            Alert.alert("Please enter an email");
+        }
+    };
+
+    // Return function
+    return (
+        <View style={styles.background}>
+
+            {/* Heading */}
+            <Text style={styles.headingText}>RESET PASSWORD</Text>
+
+            {/* Email prompt and entry field */}
+            <Text style={styles.subHeadingText}>ENTER EMAIL</Text>
+            <TextInput
+                style={styles.textInput}
+                placeholder="Email"
+                onChangeText={(value) => setEmail(value)}
+            />
+
+            {/* Action buttons */}
+            <View style={styles.buttons}>
+
+                {/* Back to login screen button */}
+                <Pressable style={styles.button}>
+                    <Text style={styles.buttonText} onPress={() => navigation.navigate("Login")}>
+                    BACK
+                    </Text>
+                </Pressable>
+
+                {/* Call handleReset function to reset password */}
+                <Pressable style={styles.button} onPress={handleReset}>
+                    <Text style={styles.buttonText}>RESET</Text>
+                </Pressable>
+            </View>
+        </View>
+    );
 }
 
 // Styling
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    backgroundColor: "#ff8101",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  textInput: {
-    padding: 2,
-    borderWidth: 1,
-    borderRadius: 9,
-    borderColor: "#ffdeb7",
-    backgroundColor: "#ffdeb7",
-    width: "50%",
-    marginVertical: 10,
-  },
-  button: {
-    backgroundColor: "#903800",
-    height: 30,
-    width: 80,
-    marginTop: 10,
-    borderRadius: 20,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  buttons: {
-    flexDirection: "row",
-    width: "50%",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "white",
-  },
-  headingText: {
-    color: "#ffe9df",
-    fontWeight: "bold",
-    fontSize: 35,
-    marginBottom: 5,
-  },
-  subHeadingText: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 15,
-  },
+    
+    // Page styling
+    background: {
+        flex: 1,
+        backgroundColor: "#ff8101",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    headingText: {
+        color: "#ffe9df",
+        fontWeight: "bold",
+        fontSize: 35,
+        marginBottom: 5,
+    },
+    subHeadingText: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 15,
+    },
+    textInput: {
+        padding: 2,
+        borderWidth: 1,
+        borderRadius: 9,
+        borderColor: "#ffdeb7",
+        backgroundColor: "#ffdeb7",
+        width: "50%",
+        marginVertical: 10,
+    },
+
+    // Button styling
+    button: {
+        backgroundColor: "#903800",
+        height: 30,
+        width: 80,
+        marginTop: 10,
+        borderRadius: 20,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttons: {
+        flexDirection: "row",
+        width: "50%",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "white",
+    },
 });
