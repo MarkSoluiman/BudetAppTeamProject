2c9df4742c3299f33512f0fc0ce248e3b84313f5
Shambhavi <107373811+Shamy152@users.noreply.github.com>
2023-06-07 10:28:28 +1200

    Adding from my other branch

--- navigation/screens/Advice.js
+++ navigation/screens/Advice.js
@@ -1,106 +1,40 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
-import axios from 'axios';
-import { FlatList } from 'react-native-gesture-handler';
+// Component imports
+import { View,StyleSheet } from 'react-native'
+import ChatBot from './Bot';
 
-const ChatBot = () => {
-  const [data, setData] = useState([]);
-  const apiKey = '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'; // API Key
-  const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions'; // URL to OpenAI
-  const [textInput, setTextInput] = useState('');
-
-  const handleSend = async () => {
-    const prompt = textInput;
-    const response = await axios.post(
-      apiUrl,
-      {
-        prompt: prompt,
-        max_tokens: 1024,
-        temperature: 0.5,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`, // API key for verification
-        },
-      }
+// Exported function
+export default function Advice({navigation}){
+    return(
+        <View style={styles.container}>
+        <ChatBot/>
+        </View>
+        
     );
-    const text = response.data.choices[0].text;
-    setData([...data, { type: 'user', text: textInput }, { type: 'bot', text: text }]);
-    setTextInput(''); // Clear the input text after receiving the response from the API
-  };
+}
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Advice Bot</Text>
-      <FlatList
-        data={data}
-        keyExtractor={(item, index) => index.toString()}
-        style={styles.body}
-        renderItem={({ item }) => (
-          <View style={{ flexDirection: 'row', padding: 10 }}>
-            <Text style={{ fontWeight: 'bold', color: item.type === 'user' ? 'green' : 'orange' }}>
-              {item.type === 'user' ? 'User' : 'Advice Bot'}  // User and bot  response and retrieve 
-            </Text>
-            <Text style={styles.bot}>{item.text}</Text>
-          </View>
-        )}
-      />
-      <TextInput
-        style={styles.input}
-        value={textInput}
-        onChangeText={text => setTextInput(text)}
-        placeholder="Please enter your question"
-      />
-      <TouchableOpacity style={styles.button} onPress={handleSend}>
-        <Text style={styles.buttonText}>Send</Text>    /// Send button 
-      </TouchableOpacity>
-    </View>
-  );
-};
-
+// Styling
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ffdeb7',
-    alignItems: 'center',
-  },
-  title: {
-    fontWeight: '600',
-    fontSize: 25,
-    marginBottom: 20,
-    marginTop: 20,
-  },
-  body: {
-    backgroundColor: '#ffdeb7',
-    width: '80%',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ff8100',
-    width: '90%',
-    height: 60,
-    marginBottom: 10,
-    borderRadius: 10,
-    paddingHorizontal: 10,
-  },
-  button: {
-    backgroundColor: '#ffdeb7',
-    width: '90%',
-    height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 10,
-    borderColor: '#ff8100',
-  },
-  buttonText: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: 'black',
-  },
-  bot: {
-    marginLeft: 5,
-  },
+    background:{
+        flex:1,
+        
+        
+    },
+    widget:{
+        marginHorizontal: 20
+        , marginBottom: 20
+        , borderRadius: 10
+        , borderColor: '#ff8100'
+        , borderWidth: 3
+        , width: 370
+        , padding: 15
+        , backgroundColor: '#ffe9de'
+        , justifyContent: 'space-evenly'
+    },
+    footerWidget:{
+        marginHorizontal: 20
+        , marginBottom: 20
+        , width: 370
+        , padding: 15
+        , justifyContent: 'space-evenly'
+    }
 });
-
-export default ChatBot;
-
---
+++ navigation/screens/ChatBot.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import axios from 'axios';
+import { FlatList } from 'react-native-gesture-handler';
+
+const ChatBot = () => {
+  const [data, setData] = useState([]);
+  const apiKey = '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'; // API Key
+  const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions'; // URL to OpenAI
+  const [textInput, setTextInput] = useState('');
+
+  const handleSend = async () => {
+    const prompt = textInput;
+    const response = await axios.post(
+      apiUrl,
+      {
+        prompt: prompt,
+        max_tokens: 1024,
+        temperature: 0.5,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`, // API key for verification
+        },
+      }
+    );
+    const text = response.data.choices[0].text;
+    setData([...data, { type: 'user', text: textInput }, { type: 'bot', text: text }]);
+    setTextInput(''); // Clear the input text after receiving the response from the API
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Advice Bot</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => index.toString()}
+        style={styles.body}
+        renderItem={({ item }) => (
+          <View style={{ flexDirection: 'row', padding: 10 }}>
+            <Text style={{ fontWeight: 'bold', color: item.type === 'user' ? 'green' : 'orange' }}>
+              {item.type === 'user' ? 'User' : 'Advice Bot'}  // User and bot  response and retrieve 
+            </Text>
+            <Text style={styles.bot}>{item.text}</Text>
+          </View>
+        )}
+      />
+      <TextInput
+        style={styles.input}
+        value={textInput}
+        onChangeText={text => setTextInput(text)}
+        placeholder="Please enter your question"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSend}>
+        <Text style={styles.buttonText}>Send</Text>    /// Send button 
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffdeb7',
+    alignItems: 'center',
+  },
+  title: {
+    fontWeight: '600',
+    fontSize: 25,
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  body: {
+    backgroundColor: '#ffdeb7',
+    width: '80%',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ff8100',
+    width: '90%',
+    height: 60,
+    marginBottom: 10,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#ffdeb7',
+    width: '90%',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    borderColor: '#ff8100',
+  },
+  buttonText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  bot: {
+    marginLeft: 5,
+  },
+});
+
+export default ChatBot;
+
